03e2f69e92076a2794ee9681c234ed051d01815d
ICP.Ninja <noreply@icp.ninja>
2025-06-27 12:18:31 +0300

    ICP.Ninja export

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}']
+};
+
+module.exports = {
+  darkMode: 'class', // Use class-based dark mode
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
